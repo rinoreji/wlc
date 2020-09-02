8bc02b7a20a609476f2352c993d8f7e744e16475
Rino Reji Cheriyan <mailrino@gmail.com>
2020-09-02 19:05:27 +0530

    Initial upload

---
+++ site.js
@@ -0,0 +1,109 @@
+const TANK_EMPTY = 70;
+const TANK_FULL = 10;
+
+var sensorData = [];
+var latestData = {};
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Water level',
+            backgroundColor: 'rgb(179, 217, 255)',
+            borderColor: 'rgb(9, 45, 93)',
+            data: []
+        }]
+    },
+
+    options: {
+        scales: {
+            yAxes: [{
+                ticks : {
+                    stepSize:5,
+                    reverse: false
+                }
+            }],
+            xAxes: [{
+                type:'time'
+            }]
+        }
+    }
+});
+
+var tank = document.getElementById('tankChart').getContext('2d');
+var tankChart = new Chart(tank, {
+    // The type of chart we want to create
+    type: 'bar',
+    // The data for our dataset
+    data: {
+        labels: ['Water'],
+        datasets: [{
+            label: 'Water level',
+            backgroundColor: 'rgb(179, 217, 255)',
+            borderColor: 'rgb(9, 45, 93)',
+            data: []
+        }]
+    },
+
+    options: {
+        scales: {
+            yAxes: [{
+                ticks : {
+                    stepSize:5,
+                    max: TANK_EMPTY,
+                    min: TANK_FULL
+                }
+            }],
+        }
+    }
+});
+
+function updateChart() {
+
+    chart.data.labels = $.map(sensorData, function(n,i){ return n.timestamp });
+    chart.data.datasets[0].data = $.map(sensorData, function(n,i){ return n.value });
+    chart.update();
+
+    tankChart.data.labels = [ new Date(latestData.timestamp).toLocaleTimeString()];
+    tankChart.data.datasets[0].data = [ TANK_EMPTY - latestData.value ];
+    tankChart.update();
+}
+
+var firebaseConfig = {
+    apiKey: "yA7qLGEgAdrgdKIKDAGcOqZQpumkn8IoAAgMtPi7",
+    // authDomain: "projectId.firebaseapp.com",
+    databaseURL: "https://rrc-ha.firebaseio.com",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var initFetch = false;
+var sensorDataRef = database.ref('sensor-data').orderByChild('timestamp').limitToLast(100);
+sensorDataRef.on('child_changed', function (snap) {
+    if (!initFetch) return;
+    latestData = convertToChartData(snap);
+    sensorData.push(latestData);
+    updateChart();
+});
+sensorDataRef.once('value', function (snapshot) {
+    initFetch = true;
+    sensorData = [];
+    snapshot.forEach(function (item) {
+        latestData = convertToChartData(item);
+        sensorData.push(latestData);
+    });
+    updateChart();
+});
+
+function convertToChartData(snapshot) {
+    var itemVal = snapshot.val();
+    console.log(itemVal);
+    return {
+        timestamp: new Date(itemVal.timestamp),
+        value: itemVal.value
+    };
+}
