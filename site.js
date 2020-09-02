30c7fb9c4654e3a06f49a7cf9080066e3d13842c
Rino Reji Cheriyan <mailrino@gmail.com>
2020-09-02 19:39:47 +0530

    style changes

--- site.js
+++ site.js
@@ -3,35 +3,6 @@ const TANK_FULL = 10;
 
 var sensorData = [];
 var latestData = {};
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Water level',
-            backgroundColor: 'rgb(179, 217, 255)',
-            borderColor: 'rgb(9, 45, 93)',
-            data: []
-        }]
-    },
-
-    options: {
-        scales: {
-            yAxes: [{
-                ticks : {
-                    stepSize:5,
-                    reverse: false
-                }
-            }],
-            xAxes: [{
-                type:'time'
-            }]
-        }
-    }
-});
 
 var tank = document.getElementById('tankChart').getContext('2d');
 var tankChart = new Chart(tank, {
@@ -44,6 +15,7 @@ var tankChart = new Chart(tank, {
             label: 'Water level',
             backgroundColor: 'rgb(179, 217, 255)',
             borderColor: 'rgb(9, 45, 93)',
+            borderWidth: 2,
             data: []
         }]
     },
@@ -51,24 +23,56 @@ var tankChart = new Chart(tank, {
     options: {
         scales: {
             yAxes: [{
-                ticks : {
-                    stepSize:5,
+                ticks: {
                     max: TANK_EMPTY,
-                    min: TANK_FULL
+                    min: TANK_FULL,
+                    display: false
                 }
             }],
         }
     }
 });
 
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+    // The data for our dataset
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Water usage',
+            backgroundColor: 'rgb(179, 217, 255)',
+            borderColor: 'rgb(9, 45, 93)',
+            data: []
+        }]
+    },
+
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    stepSize: 5,
+                    reverse: false
+                }
+            }],
+            xAxes: [{
+                type: 'time'
+            }]
+        }
+    }
+});
+
+
+
 function updateChart() {
 
-    chart.data.labels = $.map(sensorData, function(n,i){ return n.timestamp });
-    chart.data.datasets[0].data = $.map(sensorData, function(n,i){ return n.value });
+    chart.data.labels = $.map(sensorData, function (n, i) { return n.timestamp });
+    chart.data.datasets[0].data = $.map(sensorData, function (n, i) { return n.value });
     chart.update();
 
-    tankChart.data.labels = [ new Date(latestData.timestamp).toLocaleTimeString()];
-    tankChart.data.datasets[0].data = [ TANK_EMPTY - latestData.value ];
+    tankChart.data.labels = ['Water level as of: ' + new Date(latestData.timestamp).toLocaleTimeString()];
+    tankChart.data.datasets[0].data = [TANK_EMPTY - latestData.value];
     tankChart.update();
 }
 
@@ -82,7 +86,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 var initFetch = false;
-var sensorDataRef = database.ref('sensor-data').orderByChild('timestamp').limitToLast(100);
+var sensorDataRef = database.ref('sensor-data').orderByChild('timestamp').limitToLast(500);
 sensorDataRef.on('child_changed', function (snap) {
     if (!initFetch) return;
     latestData = convertToChartData(snap);
@@ -101,7 +105,7 @@ sensorDataRef.once('value', function (snapshot) {
 
 function convertToChartData(snapshot) {
     var itemVal = snapshot.val();
-    console.log(itemVal);
+    // console.log(itemVal);
     return {
         timestamp: new Date(itemVal.timestamp),
         value: itemVal.value
